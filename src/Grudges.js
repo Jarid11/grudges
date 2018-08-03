456842d7f3b60cf1696c3e8fb65d4b182ddd24e1
Jarid11 <Marinosj1995@gmail.com>
2018-08-02 21:03:29 -0500

    searchTerm added to state

--- src/Grudges.js
+++ src/Grudges.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Grudge from './Grudge';
-import './Grudges.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Grudge from "./Grudge";
+import "./Grudges.css";
 
 const contentFor = ({ person, deed }) => (person + deed).toLowerCase();
 
 class Grudges extends Component {
   state = {
-    searchTerm: '',
+    searchTerm: ""
   };
 
   updateSearchTerm = event => {
     const searchTerm = event.target.value.toLowerCase();
     this.setState({
-      searchTerm,
+      searchTerm
     });
   };
 
@@ -54,16 +54,16 @@ Grudges.propTypes = {
       person: PropTypes.string.isRequired,
       deed: PropTypes.string.isRequired,
       avenged: PropTypes.bool.isRequired,
-      id: PropTypes.string.isRequired,
-    }),
+      id: PropTypes.string.isRequired
+    })
   ).isRequired,
   onCheckOff: PropTypes.func,
-  onRemove: PropTypes.func,
+  onRemove: PropTypes.func
 };
 
 Grudges.defaultProps = {
   onCheckOff: () => {},
-  onRemove: () => {},
+  onRemove: () => {}
 };
 
 export default Grudges;
